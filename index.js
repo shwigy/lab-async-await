90225df9dc8a6156b46ff766c3138cf38fdede02
shwigy <floressj04@icloud.com>
2026-06-18 22:09:20 -0400

    finalized index.js code, creating required functions and satisfying npm test. note: I feel there is room for improvement

--- index.js
+++ index.js
@@ -9,3 +9,34 @@ async function fetchPosts() {
  
   displayPosts(posts);
 }
+
+// display posts, receives the array of posts as a parameter
+function displayPosts(posts) {
+  const postList = document.getElementById("post-list");
+ 
+  // loop through the posts list
+  posts.forEach((post) => {
+ 
+    // create a new li tag 
+    const li = document.createElement("li");
+ 
+    // create a new h1 tag 
+    const h1 = document.createElement("h1");
+    h1.textContent = post.title;
+ 
+    // create a new p tag
+    const p = document.createElement("p");
+    p.textContent = post.body;
+ 
+    // append h1 and p to li
+    li.appendChild(h1);
+    li.appendChild(p);
+ 
+    // append li to the ul#post-list
+    postList.appendChild(li);
+  });
+}
+fetchPosts();
+
+// I personally need to improve on this concept, I believe for this instance, there is definitely room for improvement
+// to keep it simple, im unsure of my mastery in this
