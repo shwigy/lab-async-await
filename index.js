94995af460ddbc13fae37671094ac76cd8e251b4
shwigy <floressj04@icloud.com>
2026-06-18 21:45:53 -0400

    created functionality to allow fetching data from external API

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+// fetch data from API using async/await
+async function fetchPosts() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+ 
+  // convert the response to JSON
+  const posts = await response.json();
+ 
+  displayPosts(posts);
+}
